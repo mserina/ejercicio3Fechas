af0da9b13c4ad27656c0671d77243f5a41a31eaf
DESKTOP-PR2BK7P\csi23-mserina <csi23-mserina@colegioaltair.net>
2024-02-05 13:23:46 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ejercicio3Date
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            DateTime fechaHoy = DateTime.Now;
+            Console.WriteLine("FECHA NORMAL");
+            Console.WriteLine(fechaHoy);
+            int numeroRestaAños = random.Next(0, 4);
+
+            DateTime fechaModificada = fechaHoy.AddDays(1).AddMonths(-24).AddYears(-numeroRestaAños);
+            Console.WriteLine(" ");
+            Console.WriteLine("FECHA MODIFICADA");
+            Console.WriteLine(fechaModificada);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("COMPARACION FECHAS");
+            int comparacionFechas = fechaModificada.CompareTo(fechaHoy);
+            if (comparacionFechas > 0)
+            {
+                Console.WriteLine("El hoy es mas pequeño");
+            }
+            else
+            {
+                Console.WriteLine("El hoy es mas grande");
+            }
+
+        }
+    }
+}
